9ebe2746e4d7e9b98d524382f867079fbec381b3
NidzaX <59400424+NidzaX@users.noreply.github.com>
2025-06-20 08:20:10 +0200

    Implement Filtering, Sorting, Searching and Pagination Functionality

---
+++ Core/Specifications/ProductSpecParams.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+namespace Core.Specifications;
+
+public class ProductSpecParams
+{
+    private const int MaxPageSize = 50;
+    private int _pageSize = 6;
+    private List<string> _brands = [];
+    private List<string> _types = [];
+    private string? _search;
+
+    public int PageIndex { get; set; } = 1;
+    public int PageSize
+    {
+        get => _pageSize;
+        set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+    }
+    public int MyProperty { get; set; }
+    public string? Sort { get; set; }
+    public List<string> Brands
+    {
+        get => _brands;
+        set
+        {
+            _brands = value.SelectMany(x => x.Split(',',
+                StringSplitOptions.RemoveEmptyEntries)).ToList();
+        }
+    }
+
+    public List<string> Types
+    {
+        get => _types;
+        set
+        {
+            _types = value.SelectMany(x => x.Split(',',
+                StringSplitOptions.RemoveEmptyEntries)).ToList();
+        }
+    }
+
+    public string Search
+    {
+        get => _search ?? "";
+        set => _search = value.ToLower();
+    }
+}
+
