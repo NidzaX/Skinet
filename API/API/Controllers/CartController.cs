9cf115bcdfd237f86023fba0a3def4204f939e78
NidzaX <59400424+NidzaX@users.noreply.github.com>
2025-06-24 05:39:23 +0200

    Set up and configure Redis
    - Store the customer cart in server memory and create the supporting service and controller

---
+++ API/API/Controllers/CartController.cs
@@ -0,0 +1,46 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartController(ICartService cartService) : BaseApiController
+    {
+        [HttpGet]
+        public async Task<ActionResult<ShoppingCart>> GetCartAsync(string id)
+        {
+            ShoppingCart? cart = await cartService.GetCartAsync(id);
+
+            return Ok(cart ?? new ShoppingCart { Id = id});
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ShoppingCart>> UpdateCart(ShoppingCart cart)
+        {
+            ShoppingCart? updatedCart = await cartService.SetCartAsync(cart);
+
+            if(updatedCart == null)
+            {
+                return BadRequest("Failed to update the cart.");
+            }
+
+            return updatedCart;
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> DeleteCart(string id)
+        {
+            bool result = await cartService.DeleteCartAsync(id);
+
+            if (!result)
+            {
+                return NotFound("Problem deleting cart.");
+            }
+
+            return Ok();
+        }
+    }
+}
